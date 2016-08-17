7b9e7956c08292e6002fae17bcf2c5c477a31740
mickedplay <mickedplay@gmail.com>
2016-08-17 08:13:07 +0200

    Initial release

---
+++ src/play/mickedplay/ctf/CTFPlugin.java
@@ -0,0 +1,10 @@
+package play.mickedplay.ctf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by mickedplay on 17.08.2016 at 08:09 CEST.
+ * You are not allowed to remove this comment.
+ */
+public class CTFPlugin extends JavaPlugin {
+}
